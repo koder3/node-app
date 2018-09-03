7b05a63aa2ef9dfd412f82373e8eb4c18f661f9e
koder3 <zamir160@yahoo.com>
2018-09-03 17:25:07 +0400

    initial commit

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const hbs = require('hbs')
+
+
+var app = express();
+
+app.set('view engine', 'hbs')
+app.use(express.static(__dirname + '/public'))
+app.use((req, res, next) => {
+    next()
+})
+app.use((req, res, next) => {
+    res.render('maintenance.hbs')
+})
+  
+hbs.registerPartials(__dirname + '/views/partials')
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear()
+})
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: 'Welcome to my site',
+       
+    })    
+})
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'A Title',
+      
+    })
+})
+
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: 'Invalid URL'
+    })
+})
+
+app.listen(3000, () => console.log("server is up on port 3000")) ;
